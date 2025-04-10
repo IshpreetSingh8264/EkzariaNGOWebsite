a78148f83b4a64b6ad6bbb37b0126a6c880d9e0c
Ishpreet Singh <ishpreetsingh8264@gmail.com>
2025-04-11 00:45:21 +0530

    added all the feauters of main

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import LoadingSpinner from '../components/common/LoadingSpinner';
 import AdminSidebar from '../components/Admin/AdminSidebar';
 import AdminNavbar from '../components/Admin/AdminNavbar';
+import LoadingScreen from '../components/common/LoadingScreen';
 
 const StudentDetails = lazy(() => import('../components/Admin/StudentDetails'));
 const EmailSection = lazy(() => import('../components/Admin/EmailSection'));
@@ -22,37 +23,38 @@ const AdminPage = () => {
   }, []);
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.3 }}
-      className={`${isMobile ? 'flex flex-col' : 'flex'} h-screen bg-[#FDFDFD]`}
-    >
-      {!isMobile && (
-        <AdminSidebar 
-          activeTab={activeTab} 
-          setActiveTab={setActiveTab}
-          isMobile={isMobile}
-        />
-      )}
-      {isMobile && (
-        <AdminNavbar 
-          activeTab={activeTab}
-          setActiveTab={setActiveTab}
-          isMenuOpen={mobileMenuOpen}
-          toggleMenu={() => setMobileMenuOpen(!mobileMenuOpen)}
-        />
-      )}
-      
-      <main className={`flex-1 overflow-y-auto bg-[#FFFFFF] p-6 transition-all duration-300 ${
-        isMobile ? 'w-full mt-16' : ' w-[calc(100vw-5rem)]  hover:w-full'
-      }`}>
-        <Suspense fallback={<LoadingSpinner />}>
-          {activeTab === 'applications' && <StudentDetails />}
-          {activeTab === 'email' && <EmailSection />}
-        </Suspense>
-      </main>
-    </motion.div>
+    <LoadingScreen>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+        className={`${isMobile ? 'flex flex-col' : 'flex'} h-screen bg-[#FDFDFD]`}
+      >
+        {!isMobile && (
+          <AdminSidebar
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+            isMobile={isMobile}
+          />
+        )}
+        {isMobile && (
+          <AdminNavbar
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+            isMenuOpen={mobileMenuOpen}
+            toggleMenu={() => setMobileMenuOpen(!mobileMenuOpen)}
+          />
+        )}
+
+        <main className={`flex-1 overflow-y-auto bg-[#FFFFFF] p-6 transition-all duration-300 ${isMobile ? 'w-full mt-16' : ' w-[calc(100vw-5rem)]  hover:w-full'
+          }`}>
+          <Suspense fallback={<LoadingSpinner />}>
+            {activeTab === 'applications' && <StudentDetails />}
+            {activeTab === 'email' && <EmailSection />}
+          </Suspense>
+        </main>
+      </motion.div>
+    </LoadingScreen>
   );
 };
 
