6ceae00731a76abef794b0bf0896237ffac75e3b
Ishpreet Singh <ishpreetsingh8264@gmail.com>
2025-04-11 01:36:30 +0530

    Updates

--- assets/proxy-C8tZiAfy.js
+++ assets/proxy-C8tZiAfy.js
@@ -1,4 +1,4 @@
-import{r as S}from"./index-B4C9QGZl.js";var te={exports:{}},mt={};/**
+import{r as S}from"./index-B3M94LYZ.js";var te={exports:{}},mt={};/**
  * @license React
  * react-jsx-runtime.production.js
  *
